e7cd611aa391e6a7cfbdc598f8bc1b5add4eb00b
23prime <23.prime.37@gmail.com>
2021-09-23 20:37:47 +0900

    Apply my coding rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,12 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    extends: ["@nuxtjs/eslint-config-typescript", "plugin:nuxt/recommended", "prettier"],
+    plugins: [],
+    // add your custom rules here
+    rules: {},
+};
