d1cf513e697b4f502366648b04b5425ad3ff909f
23prime <23.prime.37@gmail.com>
2021-10-05 16:33:53 +0900

    User TS in error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,39 +3,33 @@
         <h1 v-if="error.statusCode === 404">
             {{ pageNotFound }}
         </h1>
+
         <h1 v-else>
             {{ otherError }}
         </h1>
-        <NuxtLink to="/">Home</NuxtLink>
+
+        <nuxt-link to="/">Home</nuxt-link>
     </v-app>
 </template>
 
-<script>
-export default {
-    layout: "empty",
-    props: {
-        error: {
-            type: Object,
-            default: null,
-        },
-    },
-    data() {
-        return {
-            pageNotFound: "404 Not Found",
-            otherError: "An error occurred",
-        };
-    },
+<script lang="ts">
+import { Component, Vue, Prop } from "nuxt-property-decorator";
+
+@Component({ components: {} })
+export default class Error extends Vue {
+    public static layout = "error";
+
+    private pageNotFound = "404 Not Found";
+    private otherError = "An error occurred";
+
+    @Prop({ default: null })
+    error!: {
+        statusCode: number;
+    };
+
     head() {
         const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
-        return {
-            title,
-        };
-    },
-};
-</script>
-
-<style scoped>
-h1 {
-    font-size: 20px;
+        return { title };
+    }
 }
-</style>
+</script>
