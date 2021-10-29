ba93eed63fce4ffaf6c401c03bcf0a316f988d25
23prime <23.prime.37@gmail.com>
2021-10-29 20:31:40 +0900

    Use API to check authenticated

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -1,9 +1,17 @@
 import { Context, Middleware } from "@nuxt/types";
 import { AuthStore } from "@/store";
+import { $axios } from "@/utils/api";
 
 const authenticated: Middleware = async ({ redirect }: Context) => {
     const auth = await AuthStore.load();
     if (!auth) return redirect("/");
+
+    try {
+        await $axios.$get(`/api`);
+    } catch (error: any) {
+        AuthStore.destroy();
+        return redirect("/");
+    }
 };
 
 export default authenticated;
